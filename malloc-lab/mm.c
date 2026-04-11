58145ee50c05640961f12ea7077dd34e0d5b63ef
JeongYeonH <ubitnoa@gmail.com>
2026-04-11 10:37:54 +0900

    find_fst 작동 원활 + 양쪽 헤더 인식 원활 + free 인식 원활

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -36,12 +36,19 @@ team_t team = {
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
+#define DEBUG
+
+#ifdef DEBUG
+    #define dbg_printf(...) printf(__VA_ARGS__)
+#else
+    #define dbg_printf(...)
+#endif
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-
+#define HDRP(bp) ((char *)bp - SIZE_T_SIZE) // payload 주소값으로 header 찾기
+#define GET_SIZE(p) (*(size_t *)(p) & ~0x7) // 해당 주소의 8바이트 값 읽어서 size 도출
 
 static char *heap_listp; // heap의 시작 주소 => 1바이트 단위 계산을 위해 char로 설정
 
@@ -65,7 +72,7 @@ int mm_init(void)
     heap_listp = p + (2 * SIZE_T_SIZE); // 힙 시작 주소 설정: 사실상 payload 주소
     
     
-    printf("\nmm_init: 모든 메모리가 초기화 됩니다. 힙 시작주소: %p \n\n", heap_listp);
+    dbg_printf("\nmm_init: 모든 메모리가 초기화 됩니다. 힙 시작주소: %p (0 bytes) \n\n", heap_listp);
     return 0;
 }
 
@@ -78,26 +85,32 @@ int mm_init(void)
 void *mm_malloc(size_t size) // void와 void *는 다름 .. size_t => 메모리 크기를 표현하는 타입
 {
     int newsize = ALIGN(size + SIZE_T_SIZE);  // header를 위한 8바이트 추가
-    printf("mm_malloc: 점유 공간 | 실제 메모리 양: %8d | %8d  ", newsize, (int)size);
+    dbg_printf("mm_malloc: 점유 공간 | 실제 메모리 양: %8d | %8d ", newsize, (int)size);
+    
+    void *internal_p = find_fit(newsize);
+    if(internal_p != (void *)-1){
+        size_t curr_size = GET_SIZE(HDRP(internal_p)); // 기존 빈 공간 사이즈
+
+        *(size_t *)HDRP(internal_p) = (newsize | 1); // header에 새 size를 할당 & 할당 명시 
+        void *next_bp = (char *)internal_p + newsize;  // 남은 공간 시작점 주소값 찾기 
+        *(size_t *)HDRP(next_bp) = ((curr_size - newsize) | 0); // 시작점 주소값에 여분 size와 할당가능 명시
+        
+        dbg_printf("===> find_fit 여유공간 있음! .. header 위치: %p (%ld bytes)\n", HDRP(internal_p), (char *)HDRP(internal_p) - (char *)heap_listp); 
+        return(void *)internal_p; // 사용자가 할당할 payload 주소를 반환
+    }
     
     void *p = mem_sbrk(newsize) - SIZE_T_SIZE;  // p라는 변수에 "주소 숫자"를 저장함.
-                                                // payload 임으로 - 8 하여 header 생성 위치로 이동.
-                                                // 요 함수는 단순 영토 확장(에필로그 헤더를 넓히는) 역할을 함.
+                                                // payload 임으로 - 8 하여 header 생성 위치로 이동.                                             // 요 함수는 단순 영토 확장(에필로그 헤더를 넓히는) 역할을 함.
     if (p == (void *)-1){
-        printf("=> 공간이 부족합니다(실패)! \n");  // 에러 결과값(-1)에 따른 방어 코드
+        dbg_printf("=> 공간이 부족합니다(실패)!  \n");  // 에러 결과값(-1)에 따른 방어 코드
         return NULL;
     }   
     else
     {
-        *(size_t *)p = size; // (구) 에필로그 자리에 header를 박는 메서드
-                             // p가 가리키는 메모리 그 자체에 "블록 크기(size)"를 저장함. 
-                             // (선언* 와 이후*는 다름)
-                             // 만약 (size_t)p => 강제로 양수로 형변환 됨 
-                             // (size_t *) => 해당 p(주소값)은 size_t를 기록한 메모리의 주소값이라고 가정
-                             // * => 그리고 그 주소값으로 가
-                             // 따라서: p가 가리키는 실제 주소값에 size라는 양의 정수가 header(8바이트)에 입력됨
+        *(size_t *)p = (newsize | 1); // (구) 에필로그 자리에 header를 박는 메서드
         *(size_t *)((char *)p + newsize) = (0 | 1); // 에필로그를 뒤로 밀어서 생성
-        printf(" ===>   공간을 할당하였습니다! .. 명표(header) 위치: %p \n", p);
+
+        dbg_printf("===> 공간 할당! .. header 위치: %p (%ld bytes)\n", p, (char *)p - (char *)heap_listp);
         return (void *)((char *)p + SIZE_T_SIZE); // 다시 (char *)로 형 변환 
                                                   // size_t(8바이트) => char(1바이트) 로 형 변환
                                                   // 8 + 1 = 9번째 주소값을 반환: 데이터 입력이 시작되는 주소값
@@ -116,7 +129,7 @@ void mm_free(void *ptr) // ptr 주소 = payload 주소(header + 8)
     size_t size = *(size_t *)header; // header => size_t(정수화)
     *(size_t *)header = size & ~0x1; // 마지막 비드를 0으로 만들어 해제
     
-    printf("mm_free: 다음 공간이 해제되었습니다:      header|   size_t   ===>   %8p | %8d\n", header, (int)(size + SIZE_T_SIZE));
+    dbg_printf("mm_free: 다음 공간이 해제되었습니다:      header|   size_t ===> %8p (%d bytes)\n", header, (int)(size));
 }
 
 /*
@@ -131,16 +144,16 @@ void *mm_realloc(void *ptr, size_t size)
     size_t copySize;
 
     newptr = mm_malloc(size);
-    printf("mm_realloc: 공간 재할당 시도 ");
+    dbg_printf("mm_realloc: 공간 재할당 시도 ");
     if (newptr == NULL){
-        printf("=> 재할당 공간이 부족합니다(실패)! \n");
+        dbg_printf("=> 재할당 공간이 부족합니다(실패)! \n");
         return NULL;
     }
     copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
     if (size < copySize)
         copySize = size;
     memcpy(newptr, oldptr, copySize);
-    printf("=> newptr <-> oldptr 간 재조정이 일어났습니다! \n");
+    dbg_printf("=> newptr <-> oldptr 간 재조정이 일어났습니다! \n");
     mm_free(oldptr);
     return newptr;
 }
@@ -148,13 +161,16 @@ void *mm_realloc(void *ptr, size_t size)
 
 
 
-// implicit 구현: 시작부터 ==> max_heap 까지 순회
+// implicit 구현: 시작부터 ==> 에필로그 헤더까지 순회
 static void *find_fit(size_t put_size){
-    void *last_byte = mem_heap_hi(); // 현재까지 사용하였던 최대 header 위치
-    void *bp = heap_listp; // current 포인터 역할 => heap 시작 위치로 초기화
+    void *bp; // current 포인터 역할
 
-    while(1){
-
+    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = (char *)bp + GET_SIZE(HDRP(bp))){ // 반복문:  
+        size_t header_value = *(size_t *)HDRP(bp); // 해당 bp의 헤더의 값을 추출
+        int is_allocated = header_value & 0x1; // 해당 헤더가 할당되어 있는지 알아보기
+        if(!is_allocated && (put_size <= GET_SIZE(HDRP(bp)))){ // 할당 가능 && 사이즈 더 작을 시 포인터 반환
+            return bp; // payload로 header로 접근하려면: -8 
+        }
     }
 
     return (void *)-1; // 없을 경우 -1 반환
