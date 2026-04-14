3ded43bbd23b6dd30d1072d101187992e4223a12
JeongYeonH <ubitnoa@gmail.com>
2026-04-14 22:15:13 +0900

    seglist 진행

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -36,6 +36,7 @@ team_t team = {
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
+#define LISTLIMIT 15
 // #define DEBUG
 
 #ifdef DEBUG
@@ -60,21 +61,22 @@ team_t team = {
 #define NEXT_BLKP(bp) (((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp) (((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))) 
 
-#define PREV_A_TO_B(bp1, bp2) (*(void **)(bp1) = (bp2)) // 현 payload에 bp2 주소값 넣기
-#define NEXT_A_TO_B(bp1, bp2) (*(void **)((char *)(bp1) + WSIZE) = (bp2)) // 현 payload + 8바이트 자리에 bp2 주소값 넣기
-
+#define PREV_A_TO_B(bp1, bp2) (*(void **)(bp1) = (bp2)) // 현 payload(prev)에 bp2 주소값 넣기
+#define NEXT_A_TO_B(bp1, bp2) (*(void **)((char *)(bp1) + WSIZE) = (bp2)) // 현 payload + 8바이트 자리(next)에 bp2 주소값 넣기
+// #define GET_ROOT(class) (*(void **)((char *)(heap_listp) + (WSIZE * class))) // 해당 리스트의 root 주소값
 
 static char *heap_listp; // heap의 시작 주소 => 1바이트 단위 계산을 위해 char로 설정
-static char *rover; // next fit 용 주소
+// static char *rover; // next fit 용 주소
 static char *top_free; // 이전에 해제한 메모리의 payload 주소
+static char *segregation_list[LISTLIMIT]; // seglist 루트 주소값 저장 리스트
 
 static void *find_fit(size_t put_size); // implicit 구현: 사이 탐색 함수
 static void *coalesce(void *bp); // 인접 free 공간 병합 함수
 static void *extend_heap(size_t new_size); // 힙을 확장하는 함수
 
 static void place(void *bp, size_t new_size); // 기존 free 공간에 할당/분할 함수
-static void new_connect(void *bp); // explicit: free간 연결 함수
-static void disconnect(void *bp); // explicit: 해당 free 블럭 끊고 => 앞뒤로 연결하는 함수
+static void insert_node(void *bp, size_t size); // 노드 삽입하기
+static void delete_node(void *bp); // 노드 삭제하기
 
 /*
  * mm_init - initialize the malloc package.
@@ -84,6 +86,13 @@ int mm_init(void)
 {
     char *p = mem_sbrk(6 * WSIZE); // 48바이트 확보
     if (p == (void *)-1) return -1;
+
+    int list;
+
+    for(list = 0; list < LISTLIMIT; list++){
+        segregation_list[list] = NULL; // seg_list 초기화
+    }
+
     
     *(size_t *)p = 0; // 패딩: 모두 0으로 채우기
     
@@ -96,7 +105,7 @@ int mm_init(void)
 
     heap_listp = p + (2 * WSIZE); // 힙 시작 주소 설정: 현 payload 주소
     top_free = p + (2 * WSIZE); // 바로 이전에 해제한 메모리 주소: 현 payload 주소
-    rover = heap_listp; // 초기 rover는 heap 
+    // rover = heap_listp; // 초기 rover는 heap 
     
     dbg_printf("\nmm_init:  heap_strt: %p (0 bytes) \n\n", heap_listp);
     return 0;
@@ -107,7 +116,6 @@ int mm_init(void)
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 // 메모리를 할당하는 로직: 현재 기본적인 것만 구현되어 있음
-// 여기서 이해한 내용: malloc은 실제 모두 채워넣는 것이 아닌, header(명표)만 만들어 놓는다.
 void *mm_malloc(size_t size) // void와 void *는 다름 .. size_t => 메모리 크기를 표현하는 타입
 {
     int newsize = ALIGN(size + (2 * SIZE_T_SIZE));  //
@@ -162,7 +170,9 @@ void *mm_realloc(void *ptr, size_t size) // 기본 구현
 
     if(input_size > before_size){ // 새 size가 더 큰 상황
         if(GET_ALLOC(HDRP(next_bp)) == 0 && before_size + GET_SIZE(HDRP(next_bp)) >= input_size ){ // 뒷 블록이 빈 & 빈 공간에 여분 채우기 가능
-            disconnect(next_bp); // 인접 free 블럭을 연결리스트에서 삭제
+            // disconnect(next_bp); // 인접 free 블럭을 연결리스트에서 삭제
+            delete_node(next_bp);
+            
             size_t merge_size = before_size + GET_SIZE(HDRP(next_bp)); // 합칠 경우 총 size
             size_t extra_size = merge_size - input_size;
 
@@ -175,7 +185,8 @@ void *mm_realloc(void *ptr, size_t size) // 기본 구현
                 PUT(HDRP(remain_bp), PACK(extra_size, 0)); 
                 PUT(FTRP(remain_bp), PACK(extra_size, 0));
 
-                new_connect(remain_bp);
+                // new_connect(remain_bp);
+                insert_node(remain_bp, extra_size);
             }
             else{
                 PUT(HDRP(ptr), PACK(merge_size, 1));
@@ -223,14 +234,29 @@ void *mm_realloc(void *ptr, size_t size) // 기본 구현
 
 
 
-// explicit 구현: stack 구조 => 위에서 아래로
-static void *find_fit(size_t put_size){
-    void *bp; // current 포인터 역할: payload
+// seglist 구현: index찾기, root => 끝까지
+static void *find_fit(size_t put_size){  
+    int list = 0;
+    size_t block_size = put_size;
+    size_t class_size = put_size;
 
-    for(bp = (void *)top_free; GET_ALLOC(HDRP(bp)) == 0; bp = *(void **)(char *)(bp + WSIZE)){ // 스택 아래로 훑기
-        if(GET_SIZE(HDRP(bp)) >= put_size) return bp; // 해당 스택 칸의 size가 요구하는 size 보다 크다면?
+    while((list < LISTLIMIT - 1) && (class_size > 1)){ // 마찬가지로, index를 구함
+        class_size >>= 1;
+        list++;
     }
 
+    for(; list < LISTLIMIT; list++){ // 찾지 못했을 경우, 다음 index로 넘어감
+        void *search_bp = segregation_list[list];
+
+        while(search_bp != NULL){
+            if(GET_SIZE(HDRP(search_bp)) >= block_size){
+                return search_bp;
+            }
+
+            search_bp = *(void **)((char *)search_bp + WSIZE);
+        }
+    }
+    
     return (void *)-1; // 없을 경우 -1 반환
 }
 
@@ -247,38 +273,49 @@ static void *coalesce(void *bp){
     size_t size = GET_SIZE(HDRP(bp)); // 현 payload 기준 => 블록의 size 추출
 
     if(prev_alloc && next_alloc){ // 앞 뒤 블록이 모두 할당되어 있다면?
-        new_connect(bp); // 스택에 free블록 하나 쌓기
+        // new_connect(bp); // 스택에 free블록 하나 쌓기
+        insert_node(bp, size);
         return bp; // 변화 없음
     }
     else if(prev_alloc && !next_alloc){ // 뒤 블록만 free라면?
+        delete_node(next_bp);
+
         size += GET_SIZE(HDRP(next_bp)); // 뒤 블록의 size를 더함
         *(size_t *)HDRP(bp) = (size | 0); // 현 블록 헤더 size 확장 
         *(size_t *)FTRP(bp) = (size | 0); // 현 블록 푸터 size 확장 
 
-        disconnect(next_bp); // 뒤 free 없애기
-        new_connect(bp); // 합쳐진 free블록을 쌓기
+        // disconnect(next_bp); // 뒤 free 없애기
+        // new_connect(bp); // 합쳐진 free블록을 쌓기
+        insert_node(bp, size);
     }
     else if(!prev_alloc && next_alloc){ // 앞 블록만 free라면?
+        delete_node(prev_bp);
+        
         size += GET_SIZE(HDRP(prev_bp)); // 앞 블록의 size를 더함
         *(size_t *)HDRP(prev_bp) = (size | 0); // 앞 블록 헤더 size 확장
         *(size_t *)FTRP(prev_bp) = (size | 0); // 현 블록 푸터 size 확장
         
-        disconnect(prev_bp); // 앞 free블록을 일단 없앰
+        // disconnect(prev_bp); // 앞 free블록을 일단 없앰
         bp = prev_bp; // bp를 앞 블록 payload로 이동
-        new_connect(bp); // 합쳐진 free블록으로 쌓기
+        // new_connect(bp); // 합쳐진 free블록으로 쌓기
+        insert_node(bp, size);
     }
     else if(!prev_alloc && !next_alloc){ // 앞 뒤 블록 모두 free라면?
+        delete_node(prev_bp);
+        delete_node(next_bp);
+        
         size += (GET_SIZE(HDRP(next_bp)) + GET_SIZE(HDRP(prev_bp))); // 앞 뒤 블록의 size를 모두 더함
         *(size_t *)HDRP(prev_bp) = (size | 0); // 앞 블록의 헤더 size 확장
         *(size_t *)FTRP(prev_bp) = (size | 0); // 뒤 블록의 푸터 size 확장
         
-        disconnect(prev_bp); // 앞 free 없애기
-        disconnect(next_bp); // 뒤 free 없애기
+        // disconnect(prev_bp); // 앞 free 없애기
+        // disconnect(next_bp); // 뒤 free 없애기
         bp = prev_bp; // bp를 앞 블록 payload로 이동
-        new_connect(bp); // 앞뒤로 합쳐진 큰 free블록을 쌓기
+        // new_connect(bp); // 앞뒤로 합쳐진 큰 free블록을 쌓기
+        insert_node(bp, size);
     }
     dbg_printf("병합됨         =>  header: %p(%ld bytes)   size:%d bytes\n", HDRP(bp), HDRP(bp) - (char *)heap_listp, (int)(size));
-    rover = bp;
+    // rover = bp;
     return bp;
 }
 
@@ -312,9 +349,10 @@ static void place(void *bp, size_t new_size){ // 받는 인자: 할당 payload &
     size_t free_size = GET_SIZE(HDRP(bp)); // 먼저 free_size를 구한다
     int free_amount = free_size - new_size; // 여분이 될 free_size
 
-    disconnect(bp); // free 스택에서 연결 해제
+    // disconnect(bp); // free 스택에서 연결 해제
+    delete_node(bp);
 
-    if(free_amount >= (2 * DSIZE)){ // 여분의 공간이 16바이트 이상이라면?
+    if(free_amount >= (2 * DSIZE)){ // 여분의 공간이 32바이트 이상이라면?
         PUT(HDRP(bp), PACK(new_size, 1)); // 헤더 new_size로 갱신
         PUT(FTRP(bp), PACK(new_size, 1)); // 푸터 new_size로 갱신
 
@@ -323,7 +361,8 @@ static void place(void *bp, size_t new_size){ // 받는 인자: 할당 payload &
         PUT(HDRP(next_bp), PACK(free_amount, 0)); // 여분 헤더 생성
         PUT(FTRP(next_bp), PACK(free_amount, 0)); // 여분 푸터 생성
         
-        new_connect(next_bp); // 일단 새로 생긴 free이니..
+        // new_connect(next_bp); // 일단 새로 생긴 free이니..
+        insert_node(next_bp, free_amount);
     }
     else{ // 여분의 공간이 16 바이트 미만이라면? => 내부 단편화 감수
         PUT(HDRP(bp), PACK(free_size, 1));
@@ -333,41 +372,63 @@ static void place(void *bp, size_t new_size){ // 받는 인자: 할당 payload &
 
 
 
-// explicit: free 간 연결 정보는 payload 여분에 담는다
-static void new_connect(void *bp){
-    PREV_A_TO_B(bp, NULL); // 현 free의 payload에 null => 이 칸이 최신이며, 이전 것은 없다
-    NEXT_A_TO_B(bp, top_free); // 현 free의 (payload + 8)에 prev_free 주소 => 현 칸의 이전 칸..
-    PREV_A_TO_B(top_free, bp); // 이전 free의 prev은 현 free의 payload임
 
-    top_free = bp; // prev_free 갱신
+// seglist: 노드 삽입하기(payload, 메모리 size)
+static void insert_node(void *bp, size_t size){
+    int list = 0;
+    size_t class_size = size; // class 계산용
+
+    while((list < LISTLIMIT - 1) && (class_size > 1)){
+        class_size >>= 1; // 5: 63- ,6: 127-, 7: 255-, .... , 15: 65535-, 16: 65536>
+        list++; // 인덱스 구해짐
+    }
+
+    NEXT_A_TO_B(bp, segregation_list[list]); // 최신 블록으로 갱신
+    PREV_A_TO_B(bp, NULL);
+
+    if(segregation_list[list] != NULL){
+        PREV_A_TO_B(segregation_list[list], bp); // 이전 블록의 prev 갱신
+    }
+
+    segregation_list[list] = bp; // 최신값 => root로 갱신
 }
 
 
 
-// explicit: 특정 free 블럭을 할당처리 => 나머지 앞뒤 free 칸을 연결한다
-static void disconnect(void *bp){
-    void *prev_bp = *(void **)bp; // 이전 free블록 payload 주소값
-    void *next_bp = *(void **)((char *)bp + WSIZE); // 이후 free블록 payload 주소값
 
-    if(prev_bp == NULL && next_bp == NULL){ // 주변에 연결이 없음
-        top_free = NULL; // 스택 없음
-    }
-    else if(prev_bp == NULL && next_bp != NULL){ // 내가 맨 위
-        top_free = next_bp; // 제일 위는 이제 너야(갱신)
-        PREV_A_TO_B(next_bp, NULL); // 두 번째 위 => 제일 위
-    }
-    else if(prev_bp != NULL && next_bp == NULL){ // 내가 맨 아래
-        NEXT_A_TO_B(prev_bp, NULL); // 다음 위에게: 너가 이제 제일 아래야
-    }
-    else if(prev_bp != NULL && next_bp != NULL){ // 위 아래 중간
-        NEXT_A_TO_B(prev_bp, next_bp); // 앞뒤가 서로 직접 연결
-        PREV_A_TO_B(next_bp, prev_bp); // 동일
+// seglist: 노드 삭제하기(payload)
+static void delete_node(void *bp){
+    void *prev_bp = *(void **)bp; // 앞 블록 포인터
+    void *next_bp = *(void **)((char *)bp + WSIZE); // 뒷 블록 포인터
+    
+    int list = 0;
+    size_t block_size = GET_SIZE(HDRP(bp));
+    size_t class_size = block_size;
+
+    while((list < LISTLIMIT - 1) && (class_size > 1)){ // 마찬가지로, index를 구함
+        class_size >>= 1;
+        list++;
     }
 
-    PREV_A_TO_B(bp, NULL); // 현 free블록 prev값 없앰
-    NEXT_A_TO_B(bp, NULL); // 현 free블록 next값 없앰
+    if(prev_bp == NULL && next_bp == NULL){ // 앞 뒤 없음
+        segregation_list[list] = NULL; 
+    }
+    else if(prev_bp == NULL && next_bp != NULL){ // 앞 없음
+        segregation_list[list] = next_bp; 
+        PREV_A_TO_B(next_bp, NULL); // 뒤로 가서, 앞 칸 없음 표시
+    }
+    else if(prev_bp != NULL && next_bp == NULL){ // 뒤 없음
+        NEXT_A_TO_B(prev_bp, NULL);  // 앞으로 가서, 뒷 칸 없음 표시
+    }
+    else if(prev_bp != NULL && next_bp != NULL){ // 앞 뒤 있음
+        NEXT_A_TO_B(prev_bp, next_bp); // 배선 정리
+        PREV_A_TO_B(next_bp, prev_bp); 
+    }
+
+
+    PREV_A_TO_B(bp, NULL); // 현 prev 없앰
+    NEXT_A_TO_B(bp, NULL); // 현 next 없앰
 }
-
 
 
 
@@ -378,3 +439,4 @@ static void disconnect(void *bp){
 // explicit 통합 테스트: Perf index = 44 (util) + 40 (thru) = 84/100
 
 // realloc 수정 후: Perf index = 45 (util) + 40 (thru) = 85/100
+// seglist 통합 테스트: Perf index = 47 (util) + 40 (thru) = 87/100
